481cce77803ca21e3f15c0cc6026b59807992523
LanMuh <2938816536@qq.com>
2026-08-09 12:21:38 +0800

    Fix compact settings merge check

--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -192,7 +192,7 @@ class _SettingsPageState extends State<SettingsPage> {
         Align(
           alignment: Alignment.centerLeft,
           child: Tooltip(
-            message: AppLocalizations.of(context)!.settingsSections,
+            message: AppLocalizations.of(context)!.settings,
             child: OutlinedButton.icon(
               onPressed: () {
                 setState(() {
